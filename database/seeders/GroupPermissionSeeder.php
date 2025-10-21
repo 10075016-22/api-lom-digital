b2ce669d1e7d5005759ef81cf8011668ffe55232
Santiago Lopez <sell3.1998@hotmail.com>
2025-10-20 20:52:01 -0500

    Fix - group permission seeder was organized of correct form

--- database/seeders/GroupPermissionSeeder.php
+++ database/seeders/GroupPermissionSeeder.php
@@ -50,31 +50,35 @@ class GroupPermissionSeeder extends Seeder
             
             // Usuarios (ID: 2)
             ['group_permission_id' => 2, 'permission_id' => 3], // menu-usuarios
-            ['group_permission_id' => 2, 'permission_id' => 4], // usuarios-listar
-            ['group_permission_id' => 2, 'permission_id' => 5], // usuarios-crear
-            ['group_permission_id' => 2, 'permission_id' => 6], // usuarios-editar
-            ['group_permission_id' => 2, 'permission_id' => 7], // usuarios-eliminar
+            ['group_permission_id' => 2, 'permission_id' => 4], // usuarios-ver
+            ['group_permission_id' => 2, 'permission_id' => 5], // usuarios-listar
+            ['group_permission_id' => 2, 'permission_id' => 6], // usuarios-crear
+            ['group_permission_id' => 2, 'permission_id' => 7], // usuarios-editar
+            ['group_permission_id' => 2, 'permission_id' => 8], // usuarios-eliminar
             
             // Perfil (ID: 3)
-            ['group_permission_id' => 3, 'permission_id' => 8], // menu-perfil
-            ['group_permission_id' => 3, 'permission_id' => 9], // perfil-listar
-            ['group_permission_id' => 3, 'permission_id' => 10], // perfil-crear
-            ['group_permission_id' => 3, 'permission_id' => 11], // perfil-editar
-            ['group_permission_id' => 3, 'permission_id' => 12], // perfil-eliminar
+            ['group_permission_id' => 3, 'permission_id' => 9], // menu-perfil
+            ['group_permission_id' => 3, 'permission_id' => 10], // perfil-listar
+            ['group_permission_id' => 3, 'permission_id' => 11], // perfil-ver
+            ['group_permission_id' => 3, 'permission_id' => 12], // perfil-crear
+            ['group_permission_id' => 3, 'permission_id' => 13], // perfil-editar
+            ['group_permission_id' => 3, 'permission_id' => 14], // perfil-eliminar
             
             // Categorías (ID: 4)
-            ['group_permission_id' => 4, 'permission_id' => 13], // menu-categorias
-            ['group_permission_id' => 4, 'permission_id' => 14], // categorias-listar
-            ['group_permission_id' => 4, 'permission_id' => 15], // categorias-crear
-            ['group_permission_id' => 4, 'permission_id' => 16], // categorias-editar
-            ['group_permission_id' => 4, 'permission_id' => 17], // categorias-eliminar
+            ['group_permission_id' => 4, 'permission_id' => 15], // menu-categorias
+            ['group_permission_id' => 4, 'permission_id' => 16], // categorias-listar
+            ['group_permission_id' => 4, 'permission_id' => 17], // categorias-ver
+            ['group_permission_id' => 4, 'permission_id' => 18], // categorias-crear
+            ['group_permission_id' => 4, 'permission_id' => 19], // categorias-editar
+            ['group_permission_id' => 4, 'permission_id' => 20], // categorias-eliminar
             
             // Multimedia (ID: 5)
-            ['group_permission_id' => 5, 'permission_id' => 18], // menu-multimedia
-            ['group_permission_id' => 5, 'permission_id' => 19], // multimedia-listar
-            ['group_permission_id' => 5, 'permission_id' => 20], // multimedia-crear
-            ['group_permission_id' => 5, 'permission_id' => 21], // multimedia-editar
-            ['group_permission_id' => 5, 'permission_id' => 22], // multimedia-eliminar
+            ['group_permission_id' => 5, 'permission_id' => 21], // menu-multimedia
+            ['group_permission_id' => 5, 'permission_id' => 22], // multimedia-listar
+            ['group_permission_id' => 5, 'permission_id' => 23], // multimedia-ver
+            ['group_permission_id' => 5, 'permission_id' => 24], // multimedia-crear
+            ['group_permission_id' => 5, 'permission_id' => 25], // multimedia-editar
+            ['group_permission_id' => 5, 'permission_id' => 26], // multimedia-eliminar
         ];
 
         // Insertar relaciones en la tabla pivot
